578064488cd31059e0964b846c4598437b3cea7e
Miguel Gamboa <miguelgamboa@outlook.com>
2022-09-14 11:18:30 +0100

    Making App main more ....

--- lesson01-calendar/src/main/kotlin/pt/isel/App.kt
+++ lesson01-calendar/src/main/kotlin/pt/isel/App.kt
@@ -5,9 +5,8 @@ fun main() {
     println(NaifDate(28, 11, 2022).nextMonth()) // 12
     println(NaifDate(28, 12, 2022).nextMonth()) // 1
 
-    val d1 = NaifDate(28, 9, 2022)
-    println(d1.addDays(73))  // 10-12-2022
-    println(d1.addDays(117)) // 23-01-2023
+    println(NaifDate(28, 9, 2022).addDays(73))  // 10-12-2022
+    println(NaifDate(28, 9, 2022).addDays(117)) // 23-01-2023
 
     val d2 = NaifDate(28, 12, 2022)
     println(d2.addDays(43)) // 9-2-2023
