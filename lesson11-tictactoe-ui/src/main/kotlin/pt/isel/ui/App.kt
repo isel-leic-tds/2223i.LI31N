23b3eccfb68192cad9e8e3dee0441ae017d77a20
Miguel Gamboa <miguelgamboa@outlook.com>
2022-10-10 18:45:52 +0100

    Hw 5

--- lesson11-tictactoe-ui/src/main/kotlin/pt/isel/ui/App.kt
+++ lesson11-tictactoe-ui/src/main/kotlin/pt/isel/ui/App.kt
@@ -1,9 +1,17 @@
 package pt.isel.ui
 
 fun main() {
+/*
     readCommandsOop(mapOf<String, CommandOop>(
         "QUIT" to CmdQuitOop,
         "PLAY" to CmdPlayOop,
         "START" to CmdStartOop
     ))
+ */
+    readCommandsFp(mapOf<String, CommandFp>(
+        "QUIT" to cmdQuit,
+        "PLAY" to cmdPlay,
+        "START" to cmdStart
+    ))
+
 }
