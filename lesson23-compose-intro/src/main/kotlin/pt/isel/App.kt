c9932977d53225e0f61edadfcd3b52acea41f90d
Miguel Gamboa <miguelgamboa@outlook.com>
2022-11-07 19:50:58 +0000

    Lesson 23 - Greetings and ClickCounter components

--- lesson23-compose-intro/src/main/kotlin/pt/isel/App.kt
+++ lesson23-compose-intro/src/main/kotlin/pt/isel/App.kt
@@ -1,12 +1,12 @@
 package pt.isel
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,25 +17,29 @@ import androidx.compose.ui.window.rememberWindowState
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Compose for Desktop",
-        state = rememberWindowState(width = 300.dp, height = 300.dp)
+        title = "Compose for Desktop"
     ) {
-        val count = remember { mutableStateOf(0) }
-        MaterialTheme {
-            Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                    onClick = {
-                        count.value++
-                    }) {
-                    Text(if (count.value == 0) "Hello World" else "Clicked ${count.value}!")
-                }
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                    onClick = {
-                        count.value = 0
-                    }) {
-                    Text("Reset")
-                }
+        Column {
+            Greetings("ISEL")
+            Text("In Collumn")
+            Row {
+                Greetings("Super")
+                Text("In Row")
             }
+            val nrOfClicks: MutableState<Int> = remember { mutableStateOf(0) }
+            ClickCounter(nrOfClicks) { nrOfClicks.value += 1 }
         }
+    }
+}
+
+@Composable
+fun Greetings(name: String) {
+    Text("Hello $name")
+}
+
+@Composable
+fun ClickCounter(clicks: State<Int>, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("I've been clicked ${clicks.value} times")
     }
 }
