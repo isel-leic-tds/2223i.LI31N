df567dd3ea15d5712ed65498a20356e5b05c53af
Miguel Gamboa <miguelgamboa@outlook.com>
2022-11-10 09:52:35 +0000

    Lesson 25. Clean state of composable functions parameters.

--- lesson24-tictactoe-gui/src/main/kotlin/pt/isel/App.kt
+++ lesson24-tictactoe-gui/src/main/kotlin/pt/isel/App.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.window.*
 import pt.isel.ttt.*
+import kotlin.system.exitProcess
 
 fun main() = application {
     Window(
@@ -27,76 +30,74 @@ fun main() = application {
         )
     ) {
         MaterialTheme {
-            val board: MutableState<Board> = remember { mutableStateOf(BoardRun()) }
-            TttMenu(board)
-            GameView(board)
+            val (board, setBoard) = remember { mutableStateOf<Board>(BoardRun()) }
+            GameMenu(board, setBoard)
+            GameView(board, setBoard)
         }
     }
 }
-/**
- * Menu of the game.
- */
+
 @Composable
-fun FrameWindowScope.TttMenu(board: MutableState<Board>) {
+fun FrameWindowScope.GameMenu(board: Board, updateBoard: (Board) -> Unit) {
     MenuBar {
         Menu("Game") {
-            Item("New", onClick = { board.value = BoardRun() } )
-            Item("Quit", onClick = {  } )
+            Item("New", onClick = { updateBoard(BoardRun()) } )
+            Item("Quit", onClick = { exitProcess(0) } )
         }
     }
 }
-@Composable fun GameView(board: MutableState<Board>) {
+
+@Composable
+fun GameView(board: Board, updateBoard: (Board) -> Unit) {
         val dialogMessage = remember { mutableStateOf("") }
         val dialogVisible = remember { mutableStateOf(false) }
         BoardView(board) { pos ->
-            val local = board.value
-            if(local is BoardRun) {
-                val newBoard = local.play(pos, local.player.turn())
-                board.value = newBoard
+            if(board is BoardRun) {
+                val newBoard = board.play(pos, board.player.turn())
+                updateBoard(newBoard)
             } else {
                 dialogVisible.value = true
-                dialogMessage.value = when(local) {
+                dialogMessage.value = when(board) {
                     is BoardDraw -> "Game finished with a draw!"
-                    is BoardWin -> "Player ${local.winner} wan the game!"
+                    is BoardWin -> "Player ${board.winner} wan the game!"
                     else -> "Error unknown Game status!"
                 }
             }
         }
-        DialogMessage(dialogMessage, dialogVisible) {
+        DialogMessage(dialogMessage.value, dialogVisible.value) {
             dialogVisible.value = false
         }
 }
 
-@Composable fun DialogMessage(msg: State<String>, visible: State<Boolean>, action: () -> Unit) {
-    if(visible.value) {
+@Composable
+fun DialogMessage(msg: String, visible: Boolean, action: () -> Unit) {
+    if(visible) {
          Dialog(
             onCloseRequest = action,
             title = "Message",
-            state = DialogState( height = Dp.Unspecified, width = 350.dp)
+            state = DialogState(height = Dp.Unspecified, width = 350.dp)
         ) {
-            Text(msg.value)
+            Text(msg)
         }
     }
 }
 
-@Composable fun BoardView(board: State<Board>, action: (Position) -> Unit) {
+@Composable
+fun BoardView(board: Board, action: (Position) -> Unit) {
     Column {
         repeat(BOARD_SIZE) { lin ->
             Row {
                 repeat(BOARD_SIZE) { col ->
-                    val mov = board.value.moves.find { it.pos == Position(lin, col) }
+                    val mov = board.moves.find { it.pos == Position(lin, col) }
                     Cell(lin, col, mov?.player, action)
                 }
             }
         }
     }
 }
-@Composable fun Cell(
-    lin: Int,
-    col: Int,
-    player: Player?,
-    action: (Position) -> Unit )
-{
+
+@Composable
+fun Cell(lin: Int, col: Int, player: Player?, action: (Position) -> Unit ) {
     val symbol = player?.symbol ?: ' '
     val modifier = Modifier
             .size(width = 100.dp, height = 100.dp)
