ddb6d9599facedd1a1d78cf02a48e0f564e3821b
Miguel Gamboa <miguelgamboa@outlook.com>
2022-11-28 18:31:27 +0000

    Lesson 31 setup

---
+++ lesson31-coroutines-structured-concorrency/src/main/kotlin/pt/isel/App.kt
@@ -0,0 +1,10 @@
+package pt.isel
+
+import kotlinx.coroutines.runBlocking
+
+
+val urls = listOf( "https://github.com/", "https://stackoverflow.com/", "http://dzone.com/", "https://www.infoq.com/")
+
+fun main() = runBlocking {
+}
+
