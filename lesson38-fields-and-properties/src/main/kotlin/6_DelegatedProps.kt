29723c9127bb3fa525d2a700eebe59b12af213a1
Miguel Gamboa <miguelgamboa@outlook.com>
2022-12-13 13:15:20 +0000

    Lesson 38

--- lesson38-fields-and-properties/src/main/kotlin/6_DelegatedProps.kt
+++ lesson38-fields-and-properties/src/main/kotlin/6_DelegatedProps.kt
@@ -1,5 +1,21 @@
 import kotlin.reflect.KProperty
 
+class Example {
+    var p: String by Delegate()
+}
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name}' in $thisRef.")
+    }
+}
 
 fun main() {
+    val ex = Example()
+    println(ex.p)
+    ex.p = "ISEL"
 }
